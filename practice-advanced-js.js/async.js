edbce0720217e1daa7f624804f1657b147ff56cc
Cuz-IAm <yameame320@gmail.com>
2023-09-11 12:08:30 +0900

    async vs sync, Promise 동작 과정, async와 await, 에러 처리

--- practice-advanced-js.js/async.js
+++ practice-advanced-js.js/async.js
@@ -1,10 +1,30 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
-function readFile() {
+async function readFile() {
   let fileData;
-  fileData = fs.readFileSync("data.txt");
-  console.log("An error occured!");
-  console.log(fileData.toString());
+  //   fileData = fs.readFile("data.txt", function (error, fileData) {
+  //     console.log("File parsing done!");
+  //     console.log(fileData.toString());
+  //     //start another async task that sends the data toa database
+  //   });
+  // if(error){
+
+  // }
+  //아까랑 다른 readFile의 콜백함수 내부에 또 다른 콜백함수를 실행하는 것이 아니라
+  //Promise.then()의 인자로 콜백함수를 넘겨서 실행한다.
+  fileData = await fs
+    .readFile("data.txt")
+    .then((fileData) => {
+      console.log("File parsing done!");
+      console.log(fileData.toString());
+    })
+    .then(() => {
+      console.log("fulfilled");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   console.log("Hi there!");
 }
 
