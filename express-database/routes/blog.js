86d9610d4a2f66ed29f14f66abba27507a8cbfbf
Cuz-IAm <yameame320@gmail.com>
2023-09-15 10:19:30 +0900

    CRUD연동 추가

--- express-database/routes/blog.js
+++ express-database/routes/blog.js
@@ -5,8 +5,11 @@ router.get("/", (req, res) => {
   res.redirect("/posts");
 });
 
-router.get("/posts", (req, res) => {
-  res.render("posts-list");
+router.get("/posts", async (req, res) => {
+  const query = `SELECT post.*, author.name AS author_name FROM post INNER JOIN author ON post.author_id=author.id`;
+  const [posts] = await db.query(query);
+  console.log(posts);
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async (req, res) => {
@@ -14,5 +17,69 @@ router.get("/new-post", async (req, res) => {
   res.render("create-post", { authors: authors });
 });
 
-router.get("/");
+router.post("/posts", async (req, res) => {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query(
+    "INSERT INTO post (title, summary, body, author_id) VALUES (?)",
+    [data]
+  );
+  res.redirect("/posts");
+});
+
+router.get("/posts/:id", async (req, res) => {
+  const query = `
+  SELECT post.*, author.name as author_name, author.email as author_email
+  FROM post INNER JOIN author
+  ON post.author_id=author.id
+  WHERE post.id= ?`;
+  const [posts] = await db.query(query, [req.params.id]);
+  if (!posts || posts.length === 0) {
+    res.status(404).render("404");
+  }
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+  res.render("post-detail", { post: postData });
+});
+
+router.get("/posts/:id/edit", async (req, res) => {
+  const query = `
+  SELECT * FROM post WHERE id=?
+  `;
+  const [posts] = await db.query(query, [req.params.id]);
+  if (!posts || posts.length === 0) {
+    res.status(404).render("404");
+  }
+  res.render("update-post", { post: posts[0] });
+});
+
+router.post("/posts/:id/edit", async (req, res) => {
+  const query = `
+  UPDATE post SET title=?, summary=?, body=? 
+  WHERE id=?`;
+  await db.query(query, [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.params.id,
+  ]);
+  res.redirect("/posts");
+});
+
+router.post("/posts/:id/delete", async (req, res) => {
+  await db.query("DELETE FROM post WHERE id=?", [req.params.id]);
+  res.redirect("/posts");
+});
 module.exports = router;
