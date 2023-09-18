2d65a6a7a67da95e464601490fcf08b1cdffda50
Cuz-IAm <yameame320@gmail.com>
2023-09-18 12:15:43 +0900

    file upload complete, Ajax&fetch files added

--- practice-file-upload/app.js
+++ practice-file-upload/app.js
@@ -13,6 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 //이 미들웨어는 인코딩의 기본 모드가 x-www-form-urlencoded로 되어 있다.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use("/images", express.static("images")); // /images URI에 요청이 들어올 때에만, images폴더를 사용하도록 한다.
 
 app.use(userRoutes);
 
--- practice-file-upload/routes/users.js
+++ practice-file-upload/routes/users.js
@@ -1,30 +1,43 @@
 const express = require("express");
 const multer = require("multer");
 
+const db = require("../data/database");
+
 const storageConfig = multer.diskStorage({
+  //cb는 callback을 의미한다.
   destination: (req, file, cb) => {
-    cb();
+    //경로 설정
+    cb(null, "images"); //cb의 첫 인자는 에러, 두 번째 인자는 파일을 저장하려는 경로이다.
   },
-  filename: (req, file, cb) => {},
+  filename: (req, file, cb) => {
+    //파일 이름에 대한 옵션 설정
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const upload = multer({ dest: "images" });
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.render("profiles");
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
 router.get("/new-user", function (req, res) {
   res.render("new-user");
 });
 
-router.post("/profiles", upload.single("image"), (req, res) => {
+router.post("/profiles", upload.single("image"), async (req, res) => {
   const uploadedImageFile = req.file;
   const userData = req.body;
 
   console.log(uploadedImageFile);
   console.log(userData);
+
+  await db.getDb().collection("users").insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
 
   res.redirect("/");
 });
