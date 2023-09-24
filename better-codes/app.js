17f715a93a206d09af71df3d22732235303e4350
Cuz-IAm <yameame320@gmail.com>
2023-09-24 15:04:05 +0900

    리팩토링: 파일 분리, View part 작성

---
+++ better-codes/app.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const express = require("express");
+const session = require("express-session");
+const csrf = require("csurf");
+
+const sessionConfig = require("./config/session");
+const db = require("./data/database");
+const authRoutes = require("./routes/auth");
+const blogRoutes = require("./routes/blog");
+const authMiddleWare = require("./middlewares/auth-middleware");
+
+const mongoDbSessionStore = sessionConfig.createSessionStore(session);
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
+app.use(csrf());
+
+app.use(authMiddleWare);
+//미들웨어 함수의 반환값을 얻으려는 게 아니라 함수를 직접 실행하려고 하는 것이라서 소괄호를 붙이지 않는다.
+
+app.use(authRoutes);
+app.use(blogRoutes);
+
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
+
+db.connectToDatabase().then(function () {
+  app.listen(4788);
+});
