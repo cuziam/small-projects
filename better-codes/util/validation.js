ee91c862f859d0cbfc82f9e353f21aadc99e9956
Cuz-IAm <yameame320@gmail.com>
2023-09-26 21:32:28 +0900

    validation check is added

--- better-codes/util/validation.js
+++ better-codes/util/validation.js
@@ -2,6 +2,18 @@ function postIsValid(title, content) {
   return !(!title || !content || title.trim() === "" || content.trim() === "");
 }
 
+function authIsValid(email, confirmEmail, password) {
+  return !(
+    !email ||
+    !confirmEmail ||
+    !password ||
+    password.trim().length < 6 ||
+    email !== confirmEmail ||
+    !email.includes("@")
+  );
+}
+
 module.exports = {
   postIsValid: postIsValid,
+  authIsValid: authIsValid,
 };
